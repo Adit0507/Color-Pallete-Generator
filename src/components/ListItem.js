5e078589c34f2b9c9d38f248a9f77a159109e011
aditya-singh-skilzen <aditya.kumar@skilzen.com>
2023-07-20 19:17:06 +0530

    List item done

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import {useState} from 'react'
+import styles from "../../styles/Home.module.css"
 
-const ListItem = () => {
-  return (
-    <div>ListItem</div>
+const copy= <i className='far fa-copy'></i>
+
+const ListItem = ({ rgb, hex }) => {
+  const [copied, setCopied] = useState(false)
+  
+    const copyToClipboard = (e)=> {
+        const color = e.target.innerText
+        navigator.clipboard.writeText(color)
+    }
+
+    return (
+    <li className={styles.colorName} style={{ background: rgb }}> 
+        <span onClick={(e) => {
+            copyToClipboard(e)
+            setCopied(true)
+            setTimeout(() => {
+                setCopied(false)
+            }, 1000)
+        }}>
+            {copied ? "Copied !": hex} {copy}
+        </span>
+    </li>
   )
 }
 
