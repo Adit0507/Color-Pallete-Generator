b43d60f840b3520db26c5c7baff7a78f09388778
aditya-singh-skilzen <aditya.kumar@skilzen.com>
2023-07-20 12:41:58 +0530

    components setup

---
+++ src/components/DisplayImage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "../../styles/Home.module.css"
+
+
+const DisplayImage = ({ uploadedImage, colorPallete }) => {
+  return (
+    <div>DisplayImage</div>
+  )
+}
+
+export default DisplayImage
