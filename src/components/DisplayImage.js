3ed2bb28bd4e0b85f3a2c3b63e75479caf84c051
aditya-singh-skilzen <aditya.kumar@skilzen.com>
2023-07-21 10:17:38 +0530

    finished the components

--- src/components/DisplayImage.js
+++ src/components/DisplayImage.js
@@ -1,10 +1,41 @@
 import React from 'react'
-import styles from "../../styles/Home.module.css"
-
+import styles from "../styles/Home.module.css"
+import ListItem from './ListItem'
+// import Image from 'next/image'
 
 const DisplayImage = ({ uploadedImage, colorPallete }) => {
+  const toHex = (rgb) => {
+    let hex = Number(rgb).toString(16)
+    if (hex.length < 2) {
+      hex = "0" + hex
+    }
+    return hex
+  }
+
   return (
-    <div>DisplayImage</div>
+    <div className={styles.content}>
+      <div className='image'>
+        {uploadedImage ? (
+          <img src={uploadedImage} alt='uploaded' />
+        ) : (
+          <h2>Put an Image here </h2>
+        )}
+      </div>
+
+      {colorPallete && (
+        <ul className={styles.colors}>
+          {colorPallete.map((color, index) => {
+            const rgb = `rgb(${color.join(",")})`
+            const hex = `#${toHex(color[0])}${toHex(color[1])}${toHex(color[2])}`
+
+            console.log(rgb, hex)
+
+            return <ListItem key={index} rgb={rgb} hex={hex} />
+          })}
+        </ul>
+      )}
+
+    </div>
   )
 }
 
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,5 +1,5 @@
 import {useState} from 'react'
-import styles from "../../styles/Home.module.css"
+import styles from "../styles/Home.module.css"
 
 const copy= <i className='far fa-copy'></i>
 
